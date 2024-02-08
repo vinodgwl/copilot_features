e7862d4e5b25269b4b102464976b5d8cd1f566ea
rajkumargwl <rajkumar.vishwakarma@galaxyweblinks.in>
2024-02-08 16:58:25 +0530

    Sample code

---
+++ example.php
@@ -0,0 +1,12 @@
+<?php
+#create the php function to get the country fullname with the country code
+function getCountry($code) {
+    $countries = array('US' => 'United States', 'UK' => 'United Kingdom', 'IN' => 'India');
+    return $countries[$code];
+}
+#call the function with the country code
+echo getCountry('US');
+?>
+
+
+
